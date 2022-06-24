76a61aea87d264938e87aa2c7ae8600167f173ce
Robert Wilson <robwilson101@gmail.com>
2022-06-24 11:41:22 +0100

    feat: add chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import theme from "../theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider theme={extendTheme(theme)} resetCSS>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
-export default MyApp
+export default MyApp;
---
+++ theme.ts
@@ -0,0 +1,5 @@
+import type { ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {};
+
+export default config;
